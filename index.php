73994d8375014f26cf67f61d5939c09a9c329322
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-03-28 16:17:00 -0400

    created connection and register.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $con = mysqli_connect("localhost","root","","socail");
+    if(mysqli_connect_errno()){
+        echo "Failed to connect: " . mysqli_connect_errno();
+    }
+    $query=mysqli_query($con,"INSERT INTO test VALUES(NULL,'REECE')");
+    // $query=mysqli_query($con,"INSERT INTO test VALUES('','REECE')");
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>social media</title>
+</head>
+<body>
+    hello broody
+</body>
+</html>
